f45213c238e2a60ecaf0536be882d3b6cbb12574
sodiumspark <8516953+sodiumspark@users.noreply.github.com>
2019-07-15 23:07:17 +0530

    Fort o

---
+++ junit/src/test/java/com/test/utils/MathUtilsTest.java
@@ -0,0 +1,15 @@
+package com.test.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+		System.out.println("Test start");
+	}
+
+}
