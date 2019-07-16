632fbeddd57b7dbf5907aa85d7b6ef9cd5ae3072
Manasvi Tyagi <manasvi.tyagi@comau.com>
2019-07-16 13:06:14 +0530

    Training Session Continued

--- junit/src/test/java/com/test/utils/MathUtilsTest.java
+++ junit/src/test/java/com/test/utils/MathUtilsTest.java
@@ -2,13 +2,44 @@ package com.test.utils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathUtilsTest {
-	MathUtils mathutils = new MathUtils();
+	MathUtils mathutils;;
+
+	
+
+	@BeforeAll
+	static void beforeAll() {
+		System.out.println("@beforeAll");
+	}
+
+	@BeforeEach
+	void initialize() {
+		mathutils = new MathUtils();
+		System.out.println("@BeforeEach");
+	}
+
+
+	@AfterEach
+	void endEach() {
+		System.out.println("@AfterEach");
+	}
+	
+	@AfterAll
+	static void afterAll() {
+		System.out.println("@AfterAll");
+	}
+
+
 	@Test
 	void addTest() {
-
 
 		int expected = 2;
 		int actual = mathutils.add(1, 1);
@@ -28,10 +59,8 @@ class MathUtilsTest {
 	@Test
 	void divTest() {
 
-
-		int expected = 5;
-		int actual = mathutils.divide(10, 2);
-		assertEquals(expected, actual,"v");
+		//assertThrows(ArithmeticException.class, () -> mathutils.divide(1,0), "Divide By Zero");
+		assertThrows(NullPointerException.class, () -> mathutils.divide(1,0), "Divide By Zero");
 	}
 
 }
