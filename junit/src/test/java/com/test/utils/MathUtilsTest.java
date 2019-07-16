5d2a7b771aaef6b1d24be5d361a3f211cb962f18
Manasvi Tyagi <manasvi.tyagi@comau.com>
2019-07-16 11:58:58 +0530

    Team trining

--- junit/src/test/java/com/test/utils/MathUtilsTest.java
+++ junit/src/test/java/com/test/utils/MathUtilsTest.java
@@ -5,14 +5,33 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
-
+	MathUtils mathutils = new MathUtils();
 	@Test
-	void test() {
-	
-		MathUtils mathutils = new MathUtils();
+	void addTest() {
+
+
 		int expected = 2;
 		int actual = mathutils.add(1, 1);
-		assertEquals(expected, actual);
+		assertEquals(expected, actual,"addTest");
+	}
+
+
+	@Test
+	void multiplyTest() {
+
+
+		int expected = 6;
+		int actual = mathutils.multiply(2, 3);
+		assertEquals(expected, actual,"multiplyTest");
+	}
+
+	@Test
+	void divTest() {
+
+
+		int expected = 5;
+		int actual = mathutils.divide(10, 2);
+		assertEquals(expected, actual,"v");
 	}
 
 }
